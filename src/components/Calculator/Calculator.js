8979d4cfc221c95706b4c7e9a59035ee0be58f35
Thomas Jüsche <thomasjusche@sterrkdeta.nl>
2019-03-19 22:03:59 +0100

    Add input options to Calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+import {
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  TextField,
+  Divider,
+} from '@material-ui/core';
 import apartmentOptions from './apartmentOptions';
 import PriceTable from './PriceTable';
 
@@ -48,13 +57,70 @@ class Calculator extends Component {
     return meterPrice * selectedFloorSize;
   }
 
+  handleChange = (e, field) => {
+    const { selectedOptions } = this.state;
+    this.setState({
+      selectedOptions: {
+        ...selectedOptions,
+        [field]: +e.target.value,
+      },
+    });
+  }
+
   render() {
     // TODO: Better rounding
     const meterPrice = Math.round(this.calculateMeterPrice());
     const totalPrice = Math.round(this.calculateTotalPrice());
-    const { selectedOptions } = this.state;
+    const { selectedOptions, placement, floors } = this.state;
     return (
       <div>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Plek in gebouw</FormLabel>
+          <RadioGroup
+            aria-label="Placement"
+            name="selectedPlacement"
+            value={`${selectedOptions.selectedPlacement}`}
+            onChange={e => this.handleChange(e, 'selectedPlacement')}
+          >
+            {placement.map((place, index) => (
+              <FormControlLabel
+                value={`${index}`}
+                key={place.name}
+                control={<Radio />}
+                label={`${place.name} (${place.factor})`}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Plek in gebouw</FormLabel>
+          <RadioGroup
+            aria-label="Verdieping"
+            name="selectedFloor"
+            value={`${selectedOptions.selectedFloor}`}
+            onChange={e => this.handleChange(e, 'selectedFloor')}
+          >
+            {floors.map((place, index) => (
+              <FormControlLabel
+                value={`${index}`}
+                key={place.name}
+                control={<Radio />}
+                label={`${place.name} (${place.factor})`}
+              />
+            ))}
+          </RadioGroup>
+        </FormControl>
+        <TextField
+          id="standard-number"
+          label="Grootte woning in meters"
+          value={selectedOptions.selectedFloorSize}
+          onChange={e => this.handleChange(e, 'selectedFloorSize')}
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+        />
         <PriceTable meterPrice={meterPrice} totalPrice={totalPrice} />
       </div>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Typography } from '@material-ui/core';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Calculator from '../components/Calculator/Calculator';
@@ -7,7 +8,7 @@ import Calculator from '../components/Calculator/Calculator';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['MO*Town', 'Sloterdijk', 'Prijs']} lang="nl" />
-    <h1>Bereken hier de prijs van je woning!</h1>
+    <Typography component="h2">Bereken hier de prijs van je woning!</Typography>
     <Calculator />
   </Layout>
 );
